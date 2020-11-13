a0167c60ff4f591a15af14d5c9cfffd7d353a302
MitanshuShaBa <42906426+MitanshuShaBa@users.noreply.github.com>
2020-11-13 18:17:51 +0530

    charting and filtering

--- create_db.py
+++ create_db.py
@@ -9,7 +9,8 @@ cursor.execute("""
     CREATE TABLE IF NOT EXISTS stock (
         id INTEGER PRIMARY KEY, 
         symbol TEXT NOT NULL UNIQUE, 
-        name TEXT NOT NULL
+        name TEXT NOT NULL,
+        exchange TEXT NOT NULL
     )
 """)
 
@@ -26,5 +27,28 @@ cursor.execute("""
         FOREIGN KEY (stock_id) REFERENCES stock (id)
     )
 """)
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS strategy(
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS stock_strategy(
+        stock_id INTEGER NOT NULL,
+        strategy_id INTEGER NOT NULL,
+        FOREIGN KEY (stock_id) REFERENCES stock (id),
+        FOREIGN KEY (strategy_id) REFERENCES strategy (id)
+    )
+""")
+
+strategies = ['opening_range_breakout', 'opening_range_breakdown']
+
+for strategy in strategies:
+    cursor.execute("""
+        INSERT INTO strategy (name) VALUES (?)
+    """, (strategy,))
 
 connection.commit()
